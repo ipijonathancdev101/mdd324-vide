cf8f8295d89368025141a42009ea55ba32d465c1
LOPEZ GOMEZ Jonathan <john.informatique38@gmail.com>
2020-04-22 11:43:30 +0200

    controller fini

--- src/main/java/com/ipiecoles/java/mdd324/homepage/controller/BitcoinController.java
+++ src/main/java/com/ipiecoles/java/mdd324/homepage/controller/BitcoinController.java
@@ -1,12 +1,13 @@
 package com.ipiecoles.java.mdd324.homepage.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ipiecoles.java.mdd324.homepage.model.Bitcoin;
+import com.ipiecoles.java.mdd324.homepage.model.BitcoinResponse;
 import com.ipiecoles.java.mdd324.homepage.utils.Utils;
 import com.owlike.genson.Genson;
 import com.owlike.genson.GensonBuilder;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @RestController
@@ -20,30 +21,19 @@ public class BitcoinController {
             value = ""
     )
 
-    public Bitcoin envoyerBitcoin(@RequestBody Bitcoin bitcoinResponse) throws Exception{
+    public BitcoinResponse envoyerBitcoin(@RequestBody Bitcoin bitcoinRequestResponse) throws Exception{
 
-        String jsonBitcoin = Utils.getPageContents("https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=" + bitcoinResponse.getCurrencyList());
+
+        String jsonBitcoin = Utils.getPageContents("https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=" + bitcoinRequestResponse.getCurrencyList());
 
         // Initialise un objet Genson
         Genson genson = new GensonBuilder().useRuntimeType(true).create();
 
         //Mapper se que l'on a récupéré dans le string jsonBitcoin
-        Map<String, Object> currencies = genson.deserialize(jsonBitcoin, Map.class);
+        HashMap<String, Double> currencies = genson.deserialize(jsonBitcoin, HashMap.class);
 
-        // Objet mapper
-        ObjectMapper mapper = new ObjectMapper();
+        BitcoinResponse bitcoinResponse = new BitcoinResponse(bitcoinRequestResponse.getBitcoinAmount(), currencies);
 
-        //bitcoinResponse.setCurrencyList(HashMap<currencies, currencies.values()>);
-
-        // Test
-        //List<String> ListDevise = mapper.readValue() ;//mapper.readValues(currencies.values());
-       // List<String> ListDevise = mapper.readValues(currencies.values());
-
-
-        // Crée une liste de valeurs strings à partir de la liste currencies
-
-
-        // Crée une liste de valeurs Doubles à partir des valeurs de la liste currencies
 
         return bitcoinResponse;
 
--- src/main/java/com/ipiecoles/java/mdd324/homepage/model/BitcoinResponse.java
+++ src/main/java/com/ipiecoles/java/mdd324/homepage/model/BitcoinResponse.java
@@ -1,23 +1,19 @@
 package com.ipiecoles.java.mdd324.homepage.model;
 
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 public class BitcoinResponse {
 
-    //Liste des devises
-    List<String> devises;
-
-    //Liste des valeurs pour chaques devises
-    List<Integer> prices;
-
-    // Constructor
-
-    public BitcoinResponse(){
-
-    }
-
-    public BitcoinResponse(List<String> devises,List<Integer> prices){
-        this.devises = devises;
-        this.prices = prices;
+    Double bitcoinAmount;
+    HashMap<String, Double> currencies;
+    public BitcoinResponse(Double bitcoinAmount, HashMap<String, Double> currencies)
+    {
+        this.bitcoinAmount = bitcoinAmount;
+        for(Map.Entry<String, Double> entry : currencies.entrySet()) {
+            String key = entry.getKey();
+            Double value = entry.getValue();
+            this.currencies.put(key, value * this.bitcoinAmount);
+        }
     }
 }
