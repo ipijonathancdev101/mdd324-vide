d137c5f9c90569dea0fc264c4bdc1b689d9b3aae
LOPEZ GOMEZ Jonathan <john.informatique38@gmail.com>
2020-04-23 16:27:13 +0200

    Ajout SimmleCorsFilter

---
+++ src/main/java/com/ipiecoles/java/mdd324/homepage/SimpleCorsFilter.java
@@ -0,0 +1,32 @@
+package com.ipiecoles.java.mdd324.homepage;
+
+import org.springframework.core.MethodParameter;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+@ControllerAdvice
+public class SimpleCorsFilter implements ResponseBodyAdvice<Object> {
+
+
+    @Override
+    public boolean supports(MethodParameter returnType, Class<? extends HttpMessageConverter<?>> converterType) {
+        return true;
+    }
+
+    @Override
+    public Object beforeBodyWrite(Object body,
+                                  MethodParameter returnType,
+                                  MediaType selectedContentType,
+                                  Class<? extends HttpMessageConverter<?>> selectedConverterType,
+                                  ServerHttpRequest request,
+                                  ServerHttpResponse response) {
+        response.getHeaders().set("Access-Control-Allow-Origin", "*");
+        response.getHeaders().set("Access-Control-Allow-Credentials", "true");
+        return body;
+    }
+
+}
